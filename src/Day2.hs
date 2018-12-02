0b4c2ae4b0c58d6530fbc47c663570b969e9d6fe
Julian Birch <julian.birch+github@gmail.com>
2018-12-02 22:40:48 +0000

    Tidy version of Day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,44 +1,41 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Day2 where
 
-import qualified Parsing as P
-import Data.Foldable(foldl')
-import Data.List(sort, group, elem)
-import Control.Applicative(liftA2)
+import Data.List(elem)
 import Control.Monad(guard)
 import Data.Bool(bool)
-import Data.Maybe(listToMaybe, catMaybes)
+import Data.Maybe(catMaybes)
 import qualified Data.Map.Strict as M
 
+import qualified Parsing as P
+import qualified Utility as U
 import Utility ((<$$>))
 
 day2Text :: IO [String]
 day2Text = lines <$> (readFile $ P.adventFile 2)
 
-xxx = "oeambtcgjqnzhgkdylfapoiusr"
+countN :: (Ord a) => Int -> [[a]] -> Int
+countN n = sum . fmap ((bool 0 1 <$> elem n) . fmap length . M.elems . U.groupByKey id) 
 
-freq :: (Ord a, Eq a) => [a] -> [Int]
-freq =  fmap length . group . sort
+testInputA :: [String]
+testInputA = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
 
-countN :: (Eq a) => a -> [[a]] -> Int
-countN n = length . filter (elem n) 
+checksum :: Ord a => [[a]] -> Int
+checksum input = product $ countN <$> [2,3] <*> pure input
 
--- day1a = (countN 2 <$> (fmap.fmap) freq day2Text) * (countN 3 <$> (fmap.fmap) freq day2Text)
+day1a :: IO Int
+day1a = checksum <$> day2Text
 
 missing :: Int -> [a] -> [a]
 missing n l = (take n l) ++ (drop (n+1) l)
 
-foldWithMerge :: (Foldable t, Semigroup m, Ord k) => t (M.Map k m) -> M.Map k m 
-foldWithMerge = M.unionsWith (<>) 
-
-groupBy :: (Ord b, Foldable t, Functor t) => (a -> b) -> t a -> M.Map b [a]
-groupBy f l = foldWithMerge (g <$> l) where
-    g a = M.singleton (f a) [a]
-
 findPair :: (Foldable t) => b -> t a -> Maybe b
 findPair k vs  = k <$ guard (2 == (length vs)) 
 
-testInput = ["abcde","fghij","klmno","pqrst","fguij","axcye","wvxyz"]
+testInputB :: [String]
+testInputB = ["abcde","fghij","klmno","pqrst","fguij","axcye","wvxyz"]
 
-day2b = catMaybes <$> (M.foldMapWithKey findPair) <$$> (traverse (groupBy . missing) [0..25]) <$> day2Text
+day2b :: IO String
+day2b = (head . catMaybes) <$> (M.foldMapWithKey findPair) <$$> (traverse (U.groupByKey . missing) [0..25]) <$> day2Text
--- src/Utility.hs
+++ src/Utility.hs
@@ -1,4 +1,9 @@
-module Utility((<$$>), leftToMaybe) where
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Utility((<$$>), leftToMaybe, groupByKey) where
+
+import qualified Data.Map.Strict as M
+import Data.Foldable(foldl')
 
 leftToMaybe :: (Either a b) -> Maybe a
 leftToMaybe (Left a) = Just a
@@ -6,3 +11,10 @@ leftToMaybe _ = Nothing
 
 (<$$>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
 (<$$>) = fmap . fmap
+
+groupByKey :: forall a b t . (Ord b, Foldable t) => (a -> b) -> t a -> M.Map b [a]
+groupByKey f l = foldl' (flip r) M.empty l where
+    g :: a -> M.Map b [a]
+    g a = M.singleton (f a) [a]
+    r :: a -> M.Map b [a] -> M.Map b [a]
+    r a m = M.unionWith (++) m (g a)

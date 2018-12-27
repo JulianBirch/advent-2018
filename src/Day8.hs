0f95a6fefb560824e2bd508dc69eb2a050a2099a
Julian Birch <julian.birch+github@gmail.com>
2018-12-27 11:00:53 +0000

    Day 8 Using fix

--- src/Day8.hs
+++ src/Day8.hs
@@ -5,9 +5,10 @@
 module Day8 where
 
 import Data.Functor.Foldable(cata, Fix(..))
-import Data.Maybe(fromMaybe)
+import Data.Function(fix)
+import Data.Functor.Compose(Compose(..))
 import Utility((<$$>))
-import Text.Show.Deriving(deriveShow, deriveShow1)
+import Text.Show.Deriving(deriveShow1)
 import Safe(atMay)
 
 import qualified Text.Megaparsec as MP
@@ -18,25 +19,21 @@ import qualified Parsing as P
 data Node s = Node {
     subNodes :: [s],
     metaData :: [Integer]
-} deriving (Functor)
+} deriving (Functor, Show)
 
-deriveShow ''Node
 deriveShow1 ''Node
 
-space :: P.Parser Char
-space = MP.single ' '
-
-nodeParser :: P.Parser Char -> P.Parser (Fix Node)
-nodeParser terminator = Fix <$> do  -- I have no idea why it doesn't work if I just put (Fix . Node) inside the do
-    childNodeCount <- Le.decimal <* space
-    metaNodeCount <- Le.decimal <* space
-    childNodes <- MP.count childNodeCount (nodeParser space)
-    metaData <- P.countSepBy metaNodeCount Le.decimal space
-    _ <- terminator
+nodeParser :: P.Parser a -> P.Parser (Node a)
+nodeParser component = do
+    childNodeCount <- Le.decimal <* C.space
+    metaNodeCount <- Le.decimal <* C.space
+    childNodes <- MP.count childNodeCount (component <* C.space)
+    metaData <- P.countSepBy metaNodeCount Le.decimal C.space
     pure $ Node childNodes metaData 
 
 day8Input :: P.ParseResult IO (Fix Node)
-day8Input = P.parseAdventFile (nodeParser C.newline) 8
+day8Input = P.parseAdventFile parser 8
+    where parser = (fix $ fmap Fix . nodeParser) <* C.newline
 
 day8a :: P.ParseResult IO Integer
 day8a = cata f <$$> day8Input where
@@ -44,10 +41,8 @@ day8a = cata f <$$> day8Input where
 
 day8bReduce :: Node Integer -> Integer
 day8bReduce n | 0 == length (subNodes n) = sum (metaData n)
-              | otherwise = sum $ lookup . fromIntegral . (subtract 1) <$> (metaData n)
-                    where lookup = (fromMaybe 0) . (atMay (subNodes n))
+              | otherwise = sum . Compose $ lookup . fromIntegral . (subtract 1) <$> (metaData n)
+                    where lookup = atMay (subNodes n)
                           
 day8b :: P.ParseResult IO Integer
 day8b = cata day8bReduce <$$> day8Input
-
-
